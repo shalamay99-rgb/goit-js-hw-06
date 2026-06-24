05d75bb9629e6a517f000ac02fe66eeb9df2657c
Vlad <shalamay99@gmail.com>
2026-06-24 12:00:48 +0300

    add index file

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+const builder = new StringBuilder(".");
+
+constructor(initialValue) {
+  this.#value = initialValue;
+}
+
+getValue() {
+    return this.#value;
+}
+
+padEnd(str) {
+  this.#value = this.#value + str;
+}
+
+padStart(str) {
+  this.#value = str + this.#value;
+}
+
+padBoth(str) {
+    this.#value = str + this.#value + str;
+}
+
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
